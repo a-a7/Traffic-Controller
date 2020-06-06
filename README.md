8bc81b9b663ad520b6fbb6c61fb8636cee20450c
Amandeep <a.s.arora0407@gmail.com>
2020-06-05 21:19:23 -0400

    Started readme, cropped diagram, setup i/o for vhdl file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Traffic-Controller
+#Traffic-Controller
+
+##Background
+This project is to make a traffic controller on an FPGA using VHDL. The traffic controller will have buttons which pedestrians can press to change the lights to allow them to cross the street. A general idea of the physical system can be seen in Figure 1. The lights will be determined by the state of the machine. Meaning for this project a finite state machine can be made.
+
+![](Diagram.png)
+Figure 1: Physical representation of what the system might look like
--- TrafficController.vhd
+++ TrafficController.vhd
@@ -1,3 +1,20 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+entity trafficController is
+    port (
+    reset       : in std_logic;
+    clock       : in std_logic;
+    buttonNS    : in std_logic;     --Pedestrian button North & Southbound
+    redNS       : out std_logic;    --Traffic lights North & Southbound
+    yellowNS    : out std_logic;
+    greenNS     : out std_logic;
+    leftNS      : out std_logic;
+    buttonEW    : in std_logic;     --Pedestrian button East & Westbound
+    redEW       : out std_logic;    --Traffic lights East & Westbound
+    yellowEW    : out std_logic;
+    greenEW     : out std_logic;
+    leftEW      : out std_logic;
+    );
+end entity;
+
