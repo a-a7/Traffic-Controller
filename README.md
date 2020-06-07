a69f0352b67442c85dac0dee02058a25d45b0b23
Amandeep <a.s.arora0407@gmail.com>
2020-06-06 21:55:14 -0400

    Finished VHDL and Readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 #Traffic-Controller
 
 ##Background
-This project is to make a traffic controller on an FPGA using VHDL. The traffic controller will have buttons which pedestrians can press to change the lights to allow them to cross the street. A general idea of the physical system can be seen in Figure 1. The lights will be determined by the state of the machine. Meaning for this project a finite state machine can be made.
-
+This project is to make a traffic controller on an FPGA using VHDL. The traffic controller will have buttons which pedestrians can press to change the lights to allow them to cross the street. The general idea of the physical system can be seen in Figure 1. The lights will be determined only by the state of the machine, thus making it a [Moore finite state machine](https://en.wikipedia.org/wiki/Moore_machine).
 ![](Diagram.png)
 Figure 1: Physical representation of what the system might look like
+
+The VHDL code can be seen in TrafficController.vhd, where as if you have Quartus you can launch the project through TrafficController.qpf
--- TrafficController.vhd
+++ TrafficController.vhd
@@ -3,18 +3,71 @@ use ieee.std_logic_1164.all;
 
 entity trafficController is
     port (
-    reset       : in std_logic;
-    clock       : in std_logic;
+    reset_n, clock  : in std_logic;
     buttonNS    : in std_logic;     --Pedestrian button North & Southbound
-    redNS       : out std_logic;    --Traffic lights North & Southbound
-    yellowNS    : out std_logic;
-    greenNS     : out std_logic;
-    leftNS      : out std_logic;
+    redNS, yellowNS, greenNS, leftNS    : out std_logic;    --Traffic lights North & Southbound
     buttonEW    : in std_logic;     --Pedestrian button East & Westbound
-    redEW       : out std_logic;    --Traffic lights East & Westbound
-    yellowEW    : out std_logic;
-    greenEW     : out std_logic;
-    leftEW      : out std_logic
+    redEW, yellowEW, greenEW, leftEW    : out std_logic    --Traffic lights East & Westbound
     );
 end entity;
 
+architecture behaviour of trafficController is
+    type statetype is (NSL, NSG, NSY, R, EWL, EWG, EWY); --States (NS = North & Southbound, EW = East & Westbound, L = Left Green Light, G = Green Light, Y = Yellow Light)
+    signal state    : statetype;
+    signal afterRed : std_logic;
+
+--High level specification
+begin
+    the_fsm: process(clock, reset_n)
+    begin
+        if (reset = '0') then
+            afterRed <= '0';
+            state <= NSL;
+        elsif (clk'event and clk = 1) then
+            case state is
+                when NSL =>
+                    state <=NSG;
+                when NSG =>
+                    state <= NSY;
+                when NSY =>
+                    afterRed <= '0';
+                    state <= R;
+                when R =>
+                    if afterRed = '0' then
+                        state <= EWL;
+                    else
+                        state <= NSL;
+                    end if;
+                when EWL =>
+                    state <= EWY;
+                when EWY =>
+                    afterRed = '1';
+                    state <= R;
+            end case;
+        elsif (buttonNS = '1') then
+            state <= NSL;
+        elsif (buttonEW = '1') then
+            state <= EWL;
+        end if;
+    end process;
+
+    --Outputs
+    redNS <= '1' when state = R
+    else '0';
+    yellowNS <= '1' when state = NSY
+    else '0';
+    greenNS <= '1' when state = NSG
+    else '0';
+    leftNS <= '1' when state = NSL
+    else '0';
+
+    redEW <= '1' when state = R
+    else '0';
+    yellowEW <= '1' when state = EWY
+    else '0';
+    greenEW <= '1' when state = EWG
+    else '0';
+    leftEW <= '1' when state = EWL
+    else '0';
+
+end architecture;
