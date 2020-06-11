677e2a140a39e402a9ed6eef1f03ed3517d96d90
Amandeep <a.s.arora0407@gmail.com>
2020-06-11 19:56:58 -0400

    Made code look nicer. Still running into an error when compiling in Quartus. Can't infer registers for all of the states.

--- TrafficController.vhd
+++ TrafficController.vhd
@@ -1,75 +1,76 @@
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-entity trafficController is
-    port (
-    reset_n, clock  : in std_logic;
-    buttonNS    : in std_logic;     --Pedestrian button North & Southbound
-    redNS, yellowNS, greenNS, leftNS    : out std_logic;    --Traffic lights North & Southbound
-    buttonEW    : in std_logic;     --Pedestrian button East & Westbound
-    redEW, yellowEW, greenEW, leftEW    : out std_logic    --Traffic lights East & Westbound
-    );
-end entity;
+ENTITY trafficController IS
+	PORT (
+		reset, clock : IN std_logic;
+		buttonNS : IN std_logic; --Pedestrian button North & Southbound
+		redNS, yellowNS, greenNS, leftNS : OUT std_logic; --Traffic lights North & Southbound
+		buttonEW : IN std_logic; --Pedestrian button East & Westbound
+		redEW, yellowEW, greenEW, leftEW : OUT std_logic --Traffic lights East & Westbound
+	);
+END ENTITY;
 
-architecture behaviour of trafficController is
-    type statetype is (NSL, NSG, NSY, R, EWL, EWG, EWY); --States (NS = North & Southbound, EW = East & Westbound, L = Left Green Light, G = Green Light, Y = Yellow Light)
-    signal state    : statetype;
-    signal afterRed : std_logic;
+ARCHITECTURE behaviour OF trafficController IS
+	TYPE statetype IS (NSL, NSG, NSY, R, EWL, EWG, EWY); --States (NS = North & Southbound, EW = East & Westbound, L = Left Green Light, G = Green Light, Y = Yellow Light)
+	SIGNAL theState : statetype;
+	SIGNAL afterRed : std_logic;
 
---High level specification
-begin
-    the_fsm: process(clock, reset_n)
-    begin
-        if (reset_n = '0') then
-            afterRed <= '0';
-            state <= NSL;
-        elsif rising_edge(clock) then
-            case state is
-                when NSL =>
-                    state <=NSG;
-                when NSG =>
-                    state <= NSY;
-                when NSY =>
-                    afterRed <= '0';
-                    state <= R;
-                when R =>
-                    if afterRed = '0' then
-                        state <= EWL;
-                    else
-                        state <= NSL;
-                    end if;
-                when EWL =>
-                    state <= EWG;
-					 when EWG =>
-					     state <= EWY;
-                when EWY =>
-                    afterRed <= '1';
-                    state <= R;
-            end case;
-        elsif (buttonNS = '1') then
-            state <= NSL;
-        elsif (buttonEW = '1') then
-            state <= EWL;
-        end if;
-    end process;
+	--High level specification
+BEGIN
+	PROCESS (clock, reset, afterRed, buttonNS, buttonEW)
+	BEGIN
+		IF reset = '1' THEN
+			theState <= NSL;
+		ELSIF clock'EVENT AND clock = '1' THEN
+			CASE theState IS
+				WHEN NSL => 
+					theState <= NSG;
+				WHEN NSG => 
+					theState <= NSY;
+				WHEN NSY => 
+					afterRed <= '0';
+					theState <= R;
+				WHEN R => 
+					IF afterRed = '0' THEN
+						theState <= EWL;
+					ELSE
+						theState <= NSL;
+					END IF;
+				WHEN EWL => 
+					theState <= EWG;
+				WHEN EWG => 
+					theState <= EWY;
+				WHEN EWY => 
+					afterRed <= '1';
+					theState <= R;
+			END CASE;
+		ELSIF buttonNS = '1' THEN
+			theState <= NSL;
+		ELSIF buttonEW = '1' THEN
+			theState <= EWL;
+		ELSE
+			NULL;
+		END IF;
+	END PROCESS;
 
-    --Outputs
-    redNS <= '1' when state = R
-    else '0';
-    yellowNS <= '1' when state = NSY
-    else '0';
-    greenNS <= '1' when state = NSG
-    else '0';
-    leftNS <= '1' when state = NSL
-    else '0';
+	--Outputs
+	redNS <= '1' WHEN theState = R
+	         ELSE '0';
+	yellowNS <= '1' WHEN theState = NSY
+	            ELSE '0';
+	greenNS <= '1' WHEN theState = NSG
+	           ELSE '0';
+	leftNS <= '1' WHEN theState = NSL
+	          ELSE '0';
 
-    redEW <= '1' when state = R
-    else '0';
-    yellowEW <= '1' when state = EWY
-    else '0';
-    greenEW <= '1' when state = EWG
-    else '0';
-    leftEW <= '1' when state = EWL
-    else '0';
+	redEW <= '1' WHEN theState = R
+	         ELSE '0';
+	yellowEW <= '1' WHEN theState = EWY
+	            ELSE '0';
+	greenEW <= '1' WHEN theState = EWG
+	           ELSE '0';
+	leftEW <= '1' WHEN theState = EWL
+	          ELSE '0';
 
-end architecture;
+END ARCHITECTURE;
